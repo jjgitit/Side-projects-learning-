736f56d95a8648b2befb68f501c0935d08cbb6e0
jjgitit <jaeyeonee0320@gmail.com>
2024-06-22 14:42:55 -0700

    finished implementing all core features. Still need to style it using css

---
+++ javascript/js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "600c7438077eb5e7b94c320b6023638a";
+
+
+function geoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url).then(response => response.json()).then(data => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  }); //this is calling the url
+}
+
+function geoError() {
+  alert("can't find you location");
+}
+
+navigator.geolocation.getCurrentPosition(geoOk, geoError);
